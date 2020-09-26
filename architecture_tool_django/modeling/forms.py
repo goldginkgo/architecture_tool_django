18ff4672d1023adcb065cb89c8e760618f6672e8
Frank Dai <yongjindai09@gmail.com>
2020-09-26 17:32:03 +0800

    Adapt to django-crispy-forms for nodetype form

--- architecture_tool_django/modeling/forms.py
+++ architecture_tool_django/modeling/forms.py
@@ -1,11 +1,39 @@
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Field, Layout
+from crispy_forms.layout import HTML, Div, Field, Layout, Submit
 from django import forms
 
 from .models import Nodetype, Schema
 
 
 class NodeTypeCreateForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(NodeTypeCreateForm, self).__init__(*args, **kwargs)
+
+        self.helper = FormHelper(self)
+        self.helper.form_class = "form-horizontal"
+        self.helper.form_group_wrapper_class = "row"
+        self.helper.label_class = "col-lg-2"
+        self.helper.field_class = "col-lg-10"
+        self.helper.layout = Layout(
+            Div(
+                "key",
+                "name",
+                "description",
+                "umlType",
+                "keyFormat",
+                "inherits",
+                "folder",
+                css_class="card-body",
+            ),
+            Div(
+                Submit("submit", "Submit"),
+                HTML(
+                    '<a class="btn btn-default float-right" href="{% url "modeling:nodetype.list" %}">Cancel</a>'
+                ),
+                css_class="card-footer",
+            ),
+        )
+
     class Meta:
         model = Nodetype
         fields = (
@@ -20,6 +48,35 @@ class NodeTypeCreateForm(forms.ModelForm):
 
 
 class NodeTypeUpdateForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(NodeTypeUpdateForm, self).__init__(*args, **kwargs)
+
+        self.helper = FormHelper(self)
+        # self.helper.form_tag = False
+        self.helper.form_class = "form-horizontal"
+        self.helper.form_group_wrapper_class = "row"
+        self.helper.label_class = "col-lg-2"
+        self.helper.field_class = "col-lg-10"
+        self.helper.layout = Layout(
+            Div(
+                "key",
+                "name",
+                "description",
+                "umlType",
+                "keyFormat",
+                "inherits",
+                "folder",
+                css_class="card-body",
+            ),
+            Div(
+                Submit("submit", "Submit"),
+                HTML(
+                    '<a class="btn btn-default float-right" href="{% url "modeling:nodetype.list" %}">Cancel</a>'
+                ),
+                css_class="card-footer",
+            ),
+        )
+
     class Meta:
         model = Nodetype
         fields = (
