4a102e34c1ce8d9452b39e4ecb0cdcc186901067
Frank Dai <yongjindai09@gmail.com>
2020-09-27 15:58:02 +0800

    Initial support for user pages

--- config/urls.py
+++ config/urls.py
@@ -3,13 +3,9 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views import defaults as default_views
-from django.views.generic import TemplateView
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path(
-        "dashboard/", TemplateView.as_view(template_name="base.html"), name="dashboard"
-    ),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
