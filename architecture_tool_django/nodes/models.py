996261ff9b09c31ce8451145e41642d0691dd67e
Frank Dai <yongjindai09@gmail.com>
2020-10-03 19:38:31 +0800

    Change fields for node and edge model

--- architecture_tool_django/nodes/models.py
+++ architecture_tool_django/nodes/models.py
@@ -1,6 +1,8 @@
 from django.contrib.postgres.fields import JSONField
 from django.db import models
 
+from architecture_tool_django.modeling.models import Edgetype, Nodetype
+
 
 class Node(models.Model):
     # STATUS_CHOICES = (
@@ -16,31 +18,57 @@ class Node(models.Model):
     #     ('RETIRED', 'Retired'),
     # )
     key = models.CharField(max_length=50, primary_key=True)
-    name = models.CharField(max_length=100)
-    type = models.CharField(max_length=50)
-    status = models.CharField(
-        max_length=50,
-        blank=True,
-        null=True
-        #   choices=STATUS_CHOICES,
+    nodetype = models.ForeignKey(
+        Nodetype, on_delete=models.CASCADE, related_name="nodes"
     )
-    attributeSet = JSONField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    attributeSet = JSONField(default=dict)
+    related_nodes = models.ManyToManyField(
+        "self", through="Edge", symmetrical=False, related_name="related_to"
+    )
 
     def __str__(self):
-        return f"{self.key}({self.name})"
+        return self.key
+
+    def add_edge(self, node, edge_type):
+        edge, created = Edge.objects.get_or_create(
+            source=self, target=node, edge_type=edge_type
+        )
+        return edge
+
+    def remove_edge(self, node, edge_type):
+        Edge.objects.filter(source=self, target=node, edge_type=edge_type).delete()
+        return
+
+    def get_target_nodes_with_edgetype(self, edge_type):
+        return self.related_nodes.filter(
+            inbound_edges__edge_type=edge_type, inbound_edges__source=self
+        )
+
+    def get_source_nodes_with_edgetype(self, edge_type):
+        return self.related_to.filter(
+            outbound_edges__edge_type=edge_type, outbound_edges__target=self
+        )
 
 
 class Edge(models.Model):
-    node = models.ForeignKey(Node, on_delete=models.CASCADE, related_name="edges")
-    type = models.CharField(max_length=50)
-    target = models.CharField(max_length=50, db_index=True)
+    source = models.ForeignKey(
+        Node, on_delete=models.CASCADE, related_name="outbound_edges"
+    )
+    target = models.ForeignKey(
+        Node, on_delete=models.CASCADE, related_name="inbound_edges"
+    )
+    edge_type = models.ForeignKey(
+        Edgetype, on_delete=models.CASCADE, related_name="edges"
+    )
 
     class Meta:
         constraints = [
             models.UniqueConstraint(
-                fields=["node", "type", "target"], name="unique_edge"
+                fields=["source", "edge_type", "target"], name="unique_edge"
             )
         ]
 
     def __str__(self):
-        return f"{self.node} -> {self.target} : {self.type}"
+        return f"{self.source} -> {self.target} : {self.edge_type}"
