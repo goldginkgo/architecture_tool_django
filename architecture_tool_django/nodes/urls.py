84b8c70d39e5e5ffcd1acf51b9d068045fae1a48
Frank Dai <yongjindai09@gmail.com>
2020-10-03 23:44:16 +0800

    Initial support for nodes

--- architecture_tool_django/nodes/urls.py
+++ architecture_tool_django/nodes/urls.py
@@ -5,5 +5,22 @@ from architecture_tool_django.nodes import views
 app_name = "nodes"
 urlpatterns = [
     path("node_counts/", views.node_counts, name="node_counts"),
-    path("nodes/", views.NodeView.as_view(), name="node.list"),
+    path("nodeapi/<str:pk>/", views.get_node, name="nodeapi"),
+    path("nodes/", views.NodeListView.as_view(), name="node.list"),
+    path("nodes/create/", views.NodeCreateView.as_view(), name="node.create"),
+    path(
+        "nodes/<str:pk>/",
+        views.NodeDetailView.as_view(),
+        name="node.detail",
+    ),
+    path(
+        "nodes/<str:pk>/update/",
+        views.NodeUpdateView.as_view(),
+        name="node.update",
+    ),
+    path(
+        "nodes/<str:pk>/delete/",
+        views.NodeDeleteView.as_view(),
+        name="node.delete",
+    ),
 ]
--- architecture_tool_django/nodes/views.py
+++ architecture_tool_django/nodes/views.py
@@ -1,11 +1,19 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponse
-from django.views.generic import ListView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.http import HttpResponse, JsonResponse
+from django.urls import reverse_lazy
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    UpdateView,
+)
 
+from . import forms
 from .models import Node
-
-# from django.shortcuts import render
 
 
 @login_required(login_url="/accounts/login/")
@@ -14,7 +22,46 @@ def node_counts(request):
     return HttpResponse(count)
 
 
-class NodeView(LoginRequiredMixin, ListView):
+class NodeListView(LoginRequiredMixin, ListView):
     model = Node
     context_object_name = "node_list"
     template_name = "nodes/list.html"
+
+
+class NodeCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
+    model = Node
+    form_class = forms.NodeCreateForm
+    template_name = "nodes/create.html"
+    success_url = reverse_lazy("nodes:node.list")
+    success_message = "Node %(key)s created successfully!"
+
+
+class NodeUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+    model = Node
+    context_object_name = "node"
+    form_class = forms.NodeUpdateForm
+    template_name = "nodes/update.html"
+    success_url = reverse_lazy("nodes:node.list")
+    success_message = "Node %(key)s updated successfully!"
+
+
+class NodeDetailView(LoginRequiredMixin, DetailView):
+    model = Node
+    template_name = "nodes/detail.html"
+
+
+class NodeDeleteView(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
+    model = Node
+    success_url = reverse_lazy("nodes:node.list")
+    success_message = "Node %(key)s deleted successfully!"
+
+    def delete(self, request, *args, **kwargs):
+        obj = self.get_object()
+        messages.success(self.request, self.success_message % obj.__dict__)
+        return super(NodeDeleteView, self).delete(request, *args, **kwargs)
+
+
+@login_required(login_url="/accounts/login/")
+def get_node(request, pk):
+    attributeSet = Node.objects.get(key=pk).attributeSet
+    return JsonResponse(attributeSet)
