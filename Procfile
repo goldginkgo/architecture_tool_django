db05ac12486dad9ff38a720d9e7f1a4c79d17332
Frank Dai <yongjindai09@gmail.com>
2020-12-25 13:11:53 +0800

    Fix for django-compose

--- Procfile
+++ Procfile
@@ -1,5 +1,5 @@
 release: python manage.py migrate
 # web: python manage.py runserver 0:8080
-web: gunicorn config.wsgi:application --bind 0.0.0.0:8080
+web: python manage.py compress && gunicorn config.wsgi:application --bind 0.0.0.0:8080
 worker: celery worker --app=config.celery_app --loglevel=info
 beat: celery beat --app=config.celery_app --loglevel=info
--- config/settings/production.py
+++ config/settings/production.py
@@ -80,7 +80,7 @@ AWS_S3_CUSTOM_DOMAIN = env("DJANGO_AWS_S3_CUSTOM_DOMAIN", default=None)
 aws_s3_domain = AWS_S3_CUSTOM_DOMAIN or f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 # STATIC
 # ------------------------
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # MEDIA
 # ------------------------------------------------------------------------------
 DEFAULT_FILE_STORAGE = "architecture_tool_django.utils.storages.MediaRootS3Boto3Storage"
